8cb3840af0608278ad46f8ad0e232b1c73a5e184
Giorgia Bertacchini <giorgia.bertacchini92@gmail.com>
2022-10-21 16:00:23 +0200

    fix Quality column in service and add clean_data

--- service.py
+++ service.py
@@ -18,6 +18,8 @@ svc = bentoml.Service('activities_model', runners=[ model_runner ])
 @svc.api(
     input=PandasDataFrame(),
     output=NumpyNdarray())
+
+
 def predict(input_data: pd.DataFrame):
     """
     Example of data input: 
@@ -29,8 +31,6 @@ def predict(input_data: pd.DataFrame):
     [{"Distance (km)": 26.91, "Example": 43, "Type": "Running", "Average Speed (km/h)": 11.08, "Activity ID": 5, "Date": 6, "Duration": 6, "Calories Burned": 1266, "Climb (m)": 98, "Average Heart rate (tpm)":121}]
     """
 
-    input_data, dict_col = preprocess_activities(input_data)
-
     parameters = {
         "header": [
             "Distance (km)",
@@ -38,9 +38,26 @@ def predict(input_data: pd.DataFrame):
             "Calories Burned",
             "Climb (m)",
             "Average Heart rate (tpm)",
-            "Quality"]
+        ]
     }
     input_data = create_model_input_table(input_data, parameters)
+
+    input_data, dict_col = preprocess_activities(input_data)
     
     print("Start the prediction...")
     return model_runner.predict.run(input_data)
+
+
+def clean_data(input_data: pd.DataFrame):
+    parameters = {
+        "header": [
+            "Distance (km)",
+            "Average Speed (km/h)" ,
+            "Calories Burned",
+            "Climb (m)",
+            "Average Heart rate (tpm)",
+            "Quality"
+        ]
+    }
+    input_data = create_model_input_table(input_data, parameters)
+    return input_data
